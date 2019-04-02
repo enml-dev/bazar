654b4f316da648ecd471b2dd79a94216f3efdccb
Edel <edelml@gmail.com>
2019-04-02 10:58:23 -0400

    Migration to androidx.

--- app/src/main/java/com/enml/bazar/ui/NewItemActivity.java
+++ app/src/main/java/com/enml/bazar/ui/NewItemActivity.java
@@ -1,7 +1,7 @@
 package com.enml.bazar.ui;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.enml.bazar.R;
 
